9e50cf45b0ee7ec95c0294f97529be477cec4b9c
iqraabbasi <iqra_abbasi2000@hotmail.dk>
2021-04-27 23:43:21 +0200

    second commit

---
+++ src/Ex1.java
@@ -0,0 +1,8 @@
+
+//ex1
+
+//Inheritance is a mechanism in which one class acquires the property of another class.
+//For example, a child inherits the traits of his or hers parents.
+//With inheritance,
+// we can reuse the fields and methods of the already existing class.
+// therefore, inheritance facilitates Reusability and is an important concept of object oriented programming.
